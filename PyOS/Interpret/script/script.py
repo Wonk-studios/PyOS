1bd4fe658b6b291fae5599df53eceac029e397cd
Wonk <138169736+TheW0NK@users.noreply.github.com>
2024-08-09 01:43:41 +0000

    Had fun with test script

--- PyOS/Interpret/script/script.py
+++ PyOS/Interpret/script/script.py
@@ -340,3 +340,60 @@ time.sleep(3600)
 #here.
 
 print("I exist!")
+
+
+time.sleep(2)
+
+print("Have a password!")
+
+import random
+
+def generate_random_password(length=12, include_upper=True, include_lower=True, include_digits=True, include_special=True):
+  """Generates a random password based on given criteria."""
+
+  characters = ''
+  if include_upper:
+    characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  if include_lower:
+    characters += 'abcdefghijklmnopqrstuvwxyz'
+  if include_digits:
+    characters += '0123456789'
+  if include_special:
+    characters += '!@#$%^&*()_+-=[]{}|;:,.<>/?`~'
+
+  if len(characters) == 0:
+    raise ValueError('At least one character type must be included.')
+
+  password = ''.join(random.choice(characters) for _ in range(length))
+  return password
+
+if __name__ == '__main__':
+  password = generate_random_password()
+  print(password)
+
+
+print("And one game of RSP.")
+
+import random
+
+def play_rock_paper_scissors():
+  """Plays a game of Rock, Paper, Scissors against the computer."""
+
+  user_choice = input("Enter your choice (rock, paper, scissors): ").lower()
+  computer_choice = random.choice(['rock', 'paper', 'scissors'])
+
+  print(f"You chose {user_choice}, computer chose {computer_choice}")
+
+  if user_choice == computer_choice:
+    print("It's a tie!")
+  elif user_choice == 'rock' and computer_choice == 'scissors':
+    print("You win!")
+  elif user_choice == 'paper' and computer_choice == 'rock':
+    print("You win!")
+  elif user_choice == 'scissors' and computer_choice == 'paper':
+    print("You win!")
+  else:
+    print("You lose!")
+
+if __name__ == '__main__':
+  play_rock_paper_scissors()
