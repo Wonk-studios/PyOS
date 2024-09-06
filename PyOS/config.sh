38296038b94960793b0c97355f845f151e1f65ab
Wonk <138169736+TheW0NK@users.noreply.github.com>
2024-09-06 22:28:14 +0000

    Added config.sh to the root directory

---
+++ PyOS/config.sh
@@ -0,0 +1,15 @@
+# config.sh
+
+# general info
+PROJECT_NAME="PyOS"
+VERSION="1.0.DEV"
+AUTHOR="Wonk Studios"
+COPYRIGHT="© 2024 Wonk Studios. All rights reserved."
+LICENSE="MIT"
+
+# Additional configuration variables
+BUILD_DIR="./build"
+SOURCE_DIR="./src"
+OUTPUT_DIR="./dist"
+DEPENDENCIES="NASM"
+BUILD_TYPE="DEV"
