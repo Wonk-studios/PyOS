33be8de4ed678ee483e5b91f83085e922892cc2e
Wonk <138169736+TheW0NK@users.noreply.github.com>
2024-09-06 15:19:24 +0000

    Being bored

--- PyOS/Interpret/script/script.py
+++ PyOS/Interpret/script/script.py
@@ -1,5 +1,27 @@
-print("Hello world!")
+class HelloWorld:
+    def __init__(self):
+        self.message = "Hello world!"
 
-print("To test keyboard input:")
+    def get_message(self):
+        return self.message
 
-input('')
+class Printer:
+    def __init__(self, message):
+        self.message = message
+
+    def print_message(self):
+        for char in self.message:
+            self._print_char(char)
+
+    def _print_char(self, char):
+        print(char, end='')
+
+def main():
+    hello_world = HelloWorld()
+    message = hello_world.get_message()
+    printer = Printer(message)
+    printer.print_message()
+    print()  # To ensure the next prompt is on a new line
+
+if __name__ == "__main__":
+    main()
