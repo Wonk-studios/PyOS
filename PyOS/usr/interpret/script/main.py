1c8b95e8cf6946505c884c6df95b518d7fffed94
Adrian Ray LeDeaux <138169736+TheW0NK@users.noreply.github.com>
2024-10-25 19:55:44 -0600

    Update main.py

--- PyOS/usr/interpret/script/main.py
+++ PyOS/usr/interpret/script/main.py
@@ -1,9 +1,14 @@
 print("%main%");
 
 def get_input():
-    user_input = input("\/%interpret_main% ~&");
+    user_input = input("\/%interpret_main% -- more ~&");
+    print(user_input);
+
+def more():
+    user_input = input()
     print(user_input);
 
 if __name__ == "__main__":
+    get_input();
     while True:
-        get_input();
+        more();
